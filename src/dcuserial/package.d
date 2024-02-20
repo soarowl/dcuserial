3eaced0a2c1f5620b759d4450b8b7937cf550275
Zhuo Nengwen <soarowl@yeah.net>
2024-02-20 16:12:17 +0800

    Add compiler and Platform export.

--- src/dcuserial/package.d
+++ src/dcuserial/package.d
@@ -4,4 +4,4 @@ public import dcubuffer;
 
 public import dcuserial.attribute : Exclude, Include, EncodeOnly, DecodeOnly,
     Condition, BigEndian, LittleEndian, Var, NoLength, Length, Custom;
-public import dcuserial.serial : serialize, deserialize;
+public import dcuserial.serial : serialize, deserialize, Compiler, Platform;
